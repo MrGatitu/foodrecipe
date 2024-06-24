c325bba7b42743097697a10d337b72fd930efbdd
MrGatitu <mrgatituh@gmail.com>
2024-06-24 16:06:20 +0300

    Added SVG for Bottom Navigation Bar

--- lib/screen_2.dart
+++ lib/screen_2.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Screen2 extends StatelessWidget {
-  const Screen2({super.key});
+  Screen2({super.key});
+
+  final String assetName = '.assets/home.svg';
+  final Widget svg = SvgPicture.asset(
+    '.assets/home.svg',
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                  child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  this.assetName,
+                  width: 24,
+                  height: 24,
+                ),
+              )),
+              Expanded(
+                  child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  '.assets/bell.svg',
+                  width: 24,
+                  height: 24,
+                ),
+              )),
+              Expanded(
+                  child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  '.assets/heart.svg',
+                  width: 24,
+                  height: 24,
+                ),
+              )),
+              Expanded(
+                  child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  '.assets/user.svg',
+                  width: 24,
+                  height: 24,
+                ),
+              ))
+              //Image.asset('.assets/home.svg'),
+              //Image.asset('.assets/bell.svg'),
+              //Image.asset('.assets/heart.svg'),
+              //Image.asset('.assets/user.svg')
+            ],
+          ),
+        ),
       ),
     );
   }
