818822eced7eb8a6884a3f49d19430db3606797b
MrGatitu <mrgatituh@gmail.com>
2024-06-26 16:07:59 +0300

    Added Some meal choices to the Main body

--- lib/screen_2.dart
+++ lib/screen_2.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
 
 class Screen2 extends StatelessWidget {
-  Screen2({super.key});
-
-  final String assetName = '.assets/home.svg';
-  final Widget svg = SvgPicture.asset(
-    'assets/home.svg',
-  );
+  const Screen2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
         body: const Column(
           children: [
             SearchBar(),
+            OptionsContainer(),
+            TextCntr(),
+            RecipeContainer()
           ],
         ),
         bottomNavigationBar: BottomAppBar(
+          color: Color.fromARGB(255, 83, 83, 83),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -31,6 +32,8 @@ class Screen2 extends StatelessWidget {
                   'assets/home.svg',
                   width: 24,
                   height: 24,
+                  colorFilter: ColorFilter.mode(
+                      Color.fromARGB(255, 92, 247, 105), BlendMode.srcIn),
                 ),
               )),
               Expanded(
@@ -40,6 +43,8 @@ class Screen2 extends StatelessWidget {
                   'assets/bell.svg',
                   width: 24,
                   height: 24,
+                  colorFilter: ColorFilter.mode(
+                      Color.fromARGB(255, 92, 247, 105), BlendMode.srcIn),
                 ),
               )),
               Expanded(
@@ -49,6 +54,8 @@ class Screen2 extends StatelessWidget {
                   'assets/heart.svg',
                   width: 24,
                   height: 24,
+                  colorFilter: ColorFilter.mode(
+                      Color.fromARGB(255, 92, 247, 105), BlendMode.srcIn),
                 ),
               )),
               Expanded(
@@ -58,6 +65,8 @@ class Screen2 extends StatelessWidget {
                   'assets/user.svg',
                   width: 24,
                   height: 24,
+                  colorFilter: ColorFilter.mode(
+                      Color.fromARGB(255, 92, 247, 105), BlendMode.srcIn),
                 ),
               ))
               //Image.asset('.assets/home.svg'),
@@ -93,6 +102,8 @@ class _SearchBarState extends State<SearchBar> {
     return Padding(
       padding: const EdgeInsets.only(top: 20.0, left: 10.0),
       child: AnimSearchBar(
+        searchIconColor: Color.fromARGB(255, 92, 247, 105),
+        textFieldColor: Color.fromARGB(255, 92, 247, 105),
         width: 400,
         textController: TextEditingController(),
         onSuffixTap: () {
@@ -101,6 +112,142 @@ class _SearchBarState extends State<SearchBar> {
           });
         },
         onSubmitted: handleSubmitted,
+      ),
+    );
+  }
+}
+
+class OptionsContainer extends StatelessWidget {
+  const OptionsContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10.0, left: 5.0, right: 5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(child: Text('Breakfast')),
+                //color: Colors.red,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 159, 245, 166),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                height: 40.0,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(child: Text('Lunch')),
+                //color: Colors.red,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 159, 245, 166),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                height: 40.0,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(child: Text('Snack')),
+                //color: Colors.red,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 159, 245, 166),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                height: 40.0,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(child: Text('Dinner')),
+                //color: Colors.red,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 159, 245, 166),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                height: 40.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TextCntr extends StatelessWidget {
+  const TextCntr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 15.0, right: 185.0),
+      child: Text(
+        'Popular Meals 🥗',
+        textAlign: TextAlign.end,
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeContainer extends StatelessWidget {
+  const RecipeContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Container(
+        height: MediaQuery.of(context).size.height, // Ensure bounded height
+        child: CustomScrollView(
+          primary: false,
+          slivers: [
+            const SliverPadding(padding: EdgeInsets.all(10.0)),
+            SliverGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: AspectRatio(
+                          aspectRatio: 1, // Adjust the aspect ratio as needed
+                          child: Image.asset(
+                            'assets/Dish1.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Dish 1',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+                // Add more containers for other dishes as needed
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
